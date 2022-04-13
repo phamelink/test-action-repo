74089be84c7675adc13c6b716f8c1808970120de
Lint Action <lint-action@samuelmeuli.com>
2022-04-13 13:25:35 +0000

    Fix code style issues with Black

--- projects/flake8/file1.py
+++ projects/flake8/file1.py
@@ -2,7 +2,7 @@ var_1 = "hello"
 var_2 = "world"
 
 
-def main ():  # Whitespace error
+def main():  # Whitespace error
     print("hello " + var_2)
 
 
@@ -20,6 +20,7 @@ def multiply(num_1, num_2):
 
 def divide(num_1, num_2):
     return num_1 / num_2
+
 
 # Blank lines error
 
