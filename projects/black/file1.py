b13c01b914fe19b92e9bca60395999022ea93584
Philip Patrick Jan Hamelink <philip.hamelink@epfl.ch>
2022-04-14 11:03:17 +0200

    Bad format for python files

--- projects/black/file1.py
+++ projects/black/file1.py
@@ -2,7 +2,7 @@ var_1 = "hello"
 var_2 = "world"
 
 
-def main():  # Whitespace error
+def main ():  # Whitespace error
     print("hello " + var_2)
 
 
@@ -20,7 +20,6 @@ def multiply(num_1, num_2):
 
 def divide(num_1, num_2):
     return num_1 / num_2
-
 
 # Blank lines error
 
